b322a15806dd7d5266074c8e6f806a11f114fc12
Juliano Ezequiel <julianoezequiel@gmail.com>
2022-04-06 17:07:00 -0300

    01

--- Project_ottol/src/main/java/br/com/ot/ProjectOtApplication.java
+++ Project_ottol/src/main/java/br/com/ot/ProjectOtApplication.java
@@ -2,8 +2,10 @@ package br.com.ot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.groovy.template.GroovyTemplateAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+GroovyTemplateAutoConfiguration.class})
 public class ProjectOtApplication {
 
 	public static void main(String[] args) {
